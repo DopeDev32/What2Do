72b620df2094a6eef29f320eb599cf0af60e0f1c
Debashis Gope <82468701+DopeDev32@users.noreply.github.com>
2025-02-22 22:07:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,55 +1,87 @@
 const title = document.getElementById("title");
-const description = document.getElementById("description")
+const description = document.getElementById("description");
 const form = document.querySelector("form");
-const container = document.querySelector(".container");
+const tasksContainer = document.getElementById("tasks-container");
 
-const task = localStorage.getItem("tasks")? JSON.parse(localStorage.getItem("tasks")):[];
+const tasks = localStorage.getItem("tasks") ? JSON.parse(localStorage.getItem("tasks")) : [];
 
-showalltsk();
+showAllTasks();
 
-function showalltsk() {
-	task.forEach((value,index)=>{
-		const div = document.createElement("div")
-		div.setAttribute("class","task")
+function showAllTasks() {
+	tasksContainer.innerHTML = '';
+	tasks.forEach((task, index) => {
+		const div = document.createElement("div");
+		div.setAttribute("class", "task animate-scale");
 
-		const inerdiv = document.createElement("div")
-		div.append(inerdiv)
+		const innerDiv = document.createElement("div");
+		innerDiv.setAttribute("class", "task-content");
+		div.append(innerDiv);
 
-		const p = document.createElement("p")
-		p.innerText = value.title
-		inerdiv.append(p)
-		
-		const span = document.createElement("span")
-		span.innerText = value.description;
-		inerdiv.append(span);
+		const h3 = document.createElement("h3");
+		h3.setAttribute("class", "task-title");
+		h3.innerText = task.title;
+		innerDiv.append(h3);
 
-		const btn = document.createElement("button")
-		btn.setAttribute("class","delbtn")
-		btn.innerText = "-";
-		div.append(btn)
+		const p = document.createElement("p");
+		p.setAttribute("class", "task-description");
+		p.innerText = task.description;
+		innerDiv.append(p);
 
-		btn.addEventListener("click",()=>{
-			removtask()
-			task.splice(index,1)
-			localStorage.setItem("tasks",JSON.stringify(task));
-			showalltsk()
-		})
-		container.append(div)
-	})
+		const actionsDiv = document.createElement("div");
+		actionsDiv.setAttribute("class", "task-actions");
+		innerDiv.append(actionsDiv);
+
+		const completeBtn = document.createElement("button");
+		completeBtn.setAttribute("class", "btn-complete animate-hover");
+		completeBtn.innerHTML = '<i class="fas fa-check"></i>';
+		actionsDiv.append(completeBtn);
+
+		const editBtn = document.createElement("button");
+		editBtn.setAttribute("class", "btn-edit animate-hover");
+		editBtn.innerHTML = '<i class="fas fa-pen"></i>';
+		actionsDiv.append(editBtn);
+
+		const deleteBtn = document.createElement("button");
+		deleteBtn.setAttribute("class", "btn-delete animate-hover");
+		deleteBtn.innerHTML = '<i class="fas fa-trash"></i>';
+		actionsDiv.append(deleteBtn);
+
+		completeBtn.addEventListener("click", () => {
+			div.classList.toggle("completed");
+		});
+
+		editBtn.addEventListener("click", () => {
+			title.value = task.title;
+			description.value = task.description;
+			tasks.splice(index, 1);
+			localStorage.setItem("tasks", JSON.stringify(tasks));
+			showAllTasks();
+		});
+
+		deleteBtn.addEventListener("click", () => {
+			div.classList.add("animate-fade-out");
+			setTimeout(() => {
+				tasks.splice(index, 1);
+				localStorage.setItem("tasks", JSON.stringify(tasks));
+				showAllTasks();
+			}, 300);
+		});
+
+		tasksContainer.append(div);
+	});
 }
-function removtask(){
-	task.forEach(()=>{
-		const div = document.querySelector(".task")
-		div.remove();
-	})
-}
-form.addEventListener("submit", (e)=>{
+
+form.addEventListener("submit", (e) => {
 	e.preventDefault();
-	removtask();
-	task.push({
+	
+	tasks.push({
 		title: title.value,
 		description: description.value,
 	});
-	localStorage.setItem("tasks",JSON.stringify(task));
-	showalltsk();
-})
+	
+	localStorage.setItem("tasks", JSON.stringify(tasks));
+	showAllTasks();
+	
+	title.value = "";
+	description.value = "";
+});
